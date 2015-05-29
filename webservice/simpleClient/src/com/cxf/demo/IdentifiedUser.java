b427d25151491cb51f3b4c1e57db03e94df979bb
FloydSun <sun.sunshine@163.com>
2015-05-29 16:16:02 +0800

    webservice sample

---
+++ webservice/simpleClient/src/com/cxf/demo/IdentifiedUser.java
@@ -0,0 +1,79 @@
+
+package com.cxf.demo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>IdentifiedUser complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="IdentifiedUser"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="user" type="{http://server.hw.demo/}User" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "IdentifiedUser", propOrder = {
+    "id",
+    "user"
+})
+public class IdentifiedUser {
+
+    protected int id;
+    protected User user;
+
+    /**
+     * 获取id属性的值。
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置id属性的值。
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * 获取user属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link User }
+     *     
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * 设置user属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link User }
+     *     
+     */
+    public void setUser(User value) {
+        this.user = value;
+    }
+
+}
